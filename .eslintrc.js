fcd022e90e46176aeef673d74a0d0ddce9d75afd
VladimirPal <mail.vpal@gmail.com>
2018-09-05 10:12:12 +0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    'airbnb',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        env: 'development',
+        config: './webpack.config.base.js',
+      },
+    },
+  },
+  env: {
+    'shared-node-browser': true,
+  },
+  globals: {},
+  plugins: [],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-console': 'off',
+    'function-paren-newline': [
+      'error',
+      'consistent',
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          consistent: true,
+        },
+        ObjectPattern: {
+          multiline: true,
+        },
+        ImportDeclaration: 'always',
+        ExportDeclaration: 'always'
+    }],
+  },
+};
